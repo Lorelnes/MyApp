fa5ad5614fa67bf6c85145494c4b62c09b00f239
Lorelnes <nataananidze@gmail.com>
2025-06-22 17:48:39 +0400

    Creating MyAdapter Class

---
+++ MyAdapter.kt
@@ -0,0 +1,18 @@
+class MyAdapter(private val items: List<String>) : RecyclerView.Adapter<MyAdapter.ViewHolder>() {
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val title: TextView = view.findViewById(R.id.titleText)
+        val image: ImageView = view.findViewById(R.id.imageView)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_view, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount() = items.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.title.text = items[position]
+    }
+}
